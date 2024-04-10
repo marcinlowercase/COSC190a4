86c5b6bc64d7debd31433e371993093c6f9b0d2e
marc <marcinlowercase@gmail.com>
2024-04-09 20:40:16 -0600

    Add Exit function for Server

--- COSC190a4/src/main/java/org/example/cosc190a4/Operations.java
+++ COSC190a4/src/main/java/org/example/cosc190a4/Operations.java
@@ -5,7 +5,10 @@ import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -17,31 +20,48 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
+import java.util.Optional;
 
 public class Operations extends Application {
+
+
+    TextField log = new TextField();
+    Button showLog = new Button("Show log");
+    Button clearLog = new Button("Clear log");
+    Button exit = new Button("Exit");
 
     @Override
     public void start(Stage stage) throws Exception {
         // Three buttons
-        Button showLog = new Button("Show log");
-        Button clearLog = new Button("Clear log");
-        Button exit = new Button("Exit");
+
+        log.setPrefSize(stage.getWidth(), stage.getHeight()*0.8);
 
         HBox hBox = new HBox(showLog, clearLog, exit);
         hBox.setAlignment(Pos.CENTER);
         hBox.setSpacing(20);
 
         BorderPane borderPane = new BorderPane();
-        borderPane.setBottom(hBox);
         borderPane.setPadding(new Insets(0, 0, 20, 0));
+        borderPane.setBottom(hBox);
+        borderPane.setTop(log);
 
 
         Scene scene = new Scene(borderPane, 400, 400);
 
 
+
+        // Exit Button
+        exit.setOnAction(event -> turnOffServer());
+
+
         stage.setScene(scene);
 
         stage.show();
+
+        Thread startServerThread = new Thread(this::startServer);
+        startServerThread.start();
+
+
 //        try {
 //            ServerSocket serverSocket = new ServerSocket(9999);
 //            System.out.println("Server started.Serving at port: " + 9999);
@@ -72,13 +92,40 @@ public class Operations extends Application {
 
     }
 
+    private void turnOffServer() {
+        Alert exitAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        exitAlert.setTitle("Exit Confirmation");
+        exitAlert.setHeaderText("Exit Confirmation");
+        exitAlert.setContentText("Are you sure you want to exit?");
+        Optional<ButtonType> result = exitAlert.showAndWait();
+
+        if (result.get() == ButtonType.OK) Platform.exit();
+        else exitAlert.close();
+    }
+
     private void startServer(){
         try (ServerSocket serverSocket = new ServerSocket(9999)){
+            System.out.println("Server start in socket: " + 9999);
 
+            while(true){
+                Socket socket = serverSocket.accept();
+
+//                System.out.println("Accepted connection from: " + socket.getInetAddress().getHostAddress());
+
+                DataInputStream inputStreamFromClient = new DataInputStream(socket.getInputStream());
+
+                String messageFromClient = inputStreamFromClient.readUTF();
+
+                System.out.println(messageFromClient);
+            }
 
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+
+
+
 }
